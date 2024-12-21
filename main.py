49d8c0b20346e401dc8a878c3eb681a24325ebf7
Marusko <matussusky@gmail.com>
2024-12-21 17:22:43 +0100

    Added pca method

--- main.py
+++ main.py
@@ -10,18 +10,33 @@ compl = pd.read_csv('data/v8_complete.csv')
 #print(miss.describe())
 #print(compl.describe())
 
-def calc_mean(data):
+def calcMean(data):
     for i in range(data.shape[1]):
         col = data[:, i]
         mean = np.nanmean(col)
         col[np.isnan(col)] = mean
     return data
 
-#def pca():
-#    return new
 
-imputed = calc_mean(np.copy(miss))
-old = np.copy(imputed)
+def pca(data, n):
+    meanVec = np.mean(data, axis=0)
+    centered = data - meanVec
+    covMatrix = np.cov(centered, rowvar=False)
+    eigVal, eigVec = np.linalg.eigh(covMatrix)
+
+    sortedInd = np.argsort(eigVal)[::-1]
+    topEigVec = eigVec[:, sortedInd[:n]]
+
+    pc = centered @ topEigVec
+    reconstructed = (pc @ topEigVec.T) + meanVec
+    return reconstructed
+
+comp = 1
+tol = 0.000001
+repl = 100
+imputed = calcMean(np.copy(miss))
+mask = np.isnan(miss)
+oldObjective = float('inf')
 
 for j in range(repl):
     #pca_data = pca()
